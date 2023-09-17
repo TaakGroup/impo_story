4c51ca601bae0328c41d735300bb8caff6c1151a
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-09-17 08:54:23 +0330

    story model

--- lib/models/story_model.dart
+++ lib/models/story_model.dart
@@ -2,9 +2,11 @@ import 'package:story_view/models/story_events.dart';
 
 class StoryModel {
   final String id;
-  final String isViewed;
+  final bool isViewed;
+  final String coverImage;
+  final String text;
   final int duration;
   final List<StoryEvents> events;
 
-  StoryModel(this.isViewed, this.id, this.events, this.duration);
+  StoryModel(this.isViewed, this.id, this.events, this.duration, this.coverImage, this.text);
 }
