fbbbf7bfcba22116298e76b4b5c0db2767f8d242
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-09-21 13:07:46 +0330

    test stream

--- lib/widgets/story_video.dart
+++ lib/widgets/story_video.dart
@@ -102,8 +102,8 @@ class StoryVideoState extends State<StoryVideo> {
 
     widget.videoLoader.loadVideo(() {
       if (widget.videoLoader.state == LoadState.success) {
-        this.playerController = VideoPlayerController.file(widget.videoLoader.videoFile!);
-        VideoPlayerController.network(widget.videoLoader.url);
+        // this.playerController = VideoPlayerController.file(widget.videoLoader.videoFile!);
+        this.playerController = VideoPlayerController.network(widget.videoLoader.url);
 
         playerController!.initialize().then((v) {
           setState(() {});
