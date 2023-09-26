7d184252d4901406212eb32e0540026325528f44
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-09-26 14:08:28 +0330

    cache data

--- lib/widgets/story_view.dart
+++ lib/widgets/story_view.dart
@@ -674,7 +674,7 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
                       colors: [
-                        Colors.black.withOpacity(0.4),
+                        Colors.black.withOpacity(0.25),
                         Colors.transparent,
                       ],
                     ),
@@ -693,7 +693,7 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
                       begin: Alignment.bottomCenter,
                       end: Alignment.topCenter,
                       colors: [
-                        Colors.black.withOpacity(0.4),
+                        Colors.black.withOpacity(0.25),
                         Colors.transparent,
                       ],
                     ),
@@ -710,7 +710,7 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
                   child: Container(
                     padding: EdgeInsets.symmetric(
                       horizontal: 16,
-                      vertical: 16,
+                      vertical: 8,
                     ),
                     child: Transform.rotate(
                       angle: pi,
@@ -738,7 +738,7 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
                   child: Container(
                     padding: EdgeInsets.symmetric(
                       horizontal: 16,
-                      vertical: 32,
+                      vertical: 16,
                     ),
                     child: Directionality(
                       textDirection: TextDirection.rtl,
