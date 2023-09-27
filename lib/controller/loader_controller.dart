11c2b4b2c276a2fa3a02ce4ef305ff4afc7fc7bb
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-09-27 09:18:44 +0330

    cache data

--- lib/controller/loader_controller.dart
+++ lib/controller/loader_controller.dart
@@ -5,11 +5,15 @@ import '../models/story_model.dart';
 class LoaderController {
   static init(List<StoryModel> stories) async {
     for (var story in stories) {
-      if (story.events.first.url.split('.').last != "m3u8") {
-        final fileInfo = await DefaultCacheManager().getFileFromCache(story.events.first.url);
-        if (fileInfo == null) {
-          final downloadedFile = await DefaultCacheManager().downloadFile(story.events.first.url);
-          await DefaultCacheManager().putFile(story.events.first.url, downloadedFile.file.readAsBytesSync());
+      for (final event in story.events) {
+        if (event.url.isNotEmpty) {
+          if (event.url.split('.').last != "m3u8") {
+            final fileInfo = await DefaultCacheManager().getFileFromCache(event.url);
+            if (fileInfo == null) {
+              final downloadedFile = await DefaultCacheManager().downloadFile(event.url);
+              await DefaultCacheManager().putFile(event.url, downloadedFile.file.readAsBytesSync());
+            }
+          }
         }
       }
     }
