9b0659dd777b24b5037c4167335b6267812f4199
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-10-11 16:42:52 +0330

    Threads

--- lib/widgets/story_threads_view.dart
+++ lib/widgets/story_threads_view.dart
@@ -6,21 +6,45 @@ import '../controller/story_threads_controller.dart';
 import '../models/story_threads_model.dart';
 import 'story_view.dart';
 
-class StoryThreadsView extends StatelessWidget {
+class StoryThreadsView extends StatefulWidget {
   final StoryThreadsController controller;
   final List<StoryThreadsModel> threads;
 
   const StoryThreadsView({Key? key, required this.controller, required this.threads}) : super(key: key);
 
+
+
+  @override
+  State<StoryThreadsView> createState() => _StoryThreadsViewState();
+}
+
+class _StoryThreadsViewState extends State<StoryThreadsView> {
+
+  double currentPageValue = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    // widget.controller.pageController = PageController(initialPage: widget.initialPage);
+    //
+    // currentPageValue = widget.initialPage.toDouble();
+
+    widget.controller.pageController!.addListener(() {
+      setState(() {
+        currentPageValue = widget.controller.pageController!.page!;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView.builder(
-          controller: controller.pageController,
-          itemCount: threads.length,
+          controller: widget.controller.pageController,
+          itemCount: widget.threads.length,
           itemBuilder: (_, i) {
-            final isLeaving = (i - controller.pageController.page!) <= 0;
-            final t = (i - controller.pageController.page!);
+            final isLeaving = (i - widget.controller.pageController.page!) <= 0;
+            final t = (i - widget.controller.pageController.page!);
             final rotationY = lerpDouble(0, 30, t)!;
             const maxOpacity = 0.8;
             final num opacity =
@@ -33,12 +57,12 @@ class StoryThreadsView extends StatelessWidget {
               alignment: isLeaving ? Alignment.centerRight : Alignment.centerLeft,
               transform: transform,
               child: StoryView(
-                controller: controller.find(threads[i].id),
+                controller: widget.controller.find(widget.threads[i].id),
                 storyItems: [
-                  for (int j = 0; j < threads.length; j++)
+                  for (int j = 0; j < widget.threads.length; j++)
                     StoryItem.fromModel(
-                      threads[i].stories[j],
-                      controller.find(threads[i].id),
+                      widget.threads[i].stories[j],
+                      widget.controller.find(widget.threads[i].id),
                     )
                 ],
               ),
