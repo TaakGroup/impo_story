e4d3bd027913a36dca325de3fe9ee0827114f88c
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-10-11 16:28:13 +0330

    Threads

--- lib/widgets/story_threads_view.dart
+++ lib/widgets/story_threads_view.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import '../controller/story_threads_controller.dart';
 import '../models/story_threads_model.dart';
@@ -13,18 +16,34 @@ class StoryThreadsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView.builder(
-        controller: controller.pageController,
-        itemCount: threads.length,
-        itemBuilder: (_, i) => StoryView(
-          controller: controller.find(threads[i].id),
-          storyItems: [
-            for (int j = 0; j < threads.length; j++)
-              StoryItem.fromModel(
-                threads[i].stories[j],
-                controller.find(threads[i].id),
-              )
-          ],
-        ),
+          controller: controller.pageController,
+          itemCount: threads.length,
+          itemBuilder: (_, i) {
+            final isLeaving = (i - controller.pageController.page!) <= 0;
+            final t = (i - controller.pageController.page!);
+            final rotationY = lerpDouble(0, 30, t)!;
+            const maxOpacity = 0.8;
+            final num opacity =
+            lerpDouble(0, maxOpacity, t.abs())!.clamp(0.0, maxOpacity);
+            final isPaging = opacity != maxOpacity;
+            final transform = Matrix4.identity();
+            transform.setEntry(3, 2, 0.003);
+            transform.rotateY(-rotationY * (pi / 180.0));
+            return Transform(
+              alignment: isLeaving ? Alignment.centerRight : Alignment.centerLeft,
+              transform: transform,
+              child: StoryView(
+                controller: controller.find(threads[i].id),
+                storyItems: [
+                  for (int j = 0; j < threads.length; j++)
+                    StoryItem.fromModel(
+                      threads[i].stories[j],
+                      controller.find(threads[i].id),
+                    )
+                ],
+              ),
+            );
+          }
       ),
     );
   }
