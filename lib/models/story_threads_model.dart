a4ee7218c381228532ce9fd4cd2fa4e8c4246990
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-10-15 10:15:58 +0330

    Threads

--- lib/models/story_threads_model.dart
+++ lib/models/story_threads_model.dart
@@ -1,12 +1,27 @@
-
+import 'package:equatable/equatable.dart';
 import 'package:story_view/models/story_model.dart';
 
-class StoryThreadsModel {
+class StoryThreadsModel extends Equatable {
   late String id;
   late List<StoryModel> stories;
 
   StoryThreadsModel.fromJson(Map<String, dynamic> json) {
     this.id = json["id"];
-    this.stories = [for(var mapJson in json['stories']) StoryModel.fromJson(mapJson)];
+    this.stories = [for (var mapJson in json['stories']) StoryModel.fromJson(mapJson)];
   }
+
+  bool get seen {
+    bool flag = false;
+    for (var story in stories) {
+      if (story.isViewed) {
+        flag = true;
+        break;
+      }
+    }
+
+    return flag;
+  }
+
+  @override
+  List<Object?> get props => [seen];
 }
