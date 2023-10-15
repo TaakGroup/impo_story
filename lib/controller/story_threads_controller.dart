142d5d76cb317214506edac4e3d60d3d1932c19a
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-10-15 08:52:02 +0330

    Threads

--- lib/controller/story_threads_controller.dart
+++ lib/controller/story_threads_controller.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_carousel_slider/carousel_slider.dart';
 import 'package:story_view/controller/story_controller.dart';
 import 'package:story_view/models/story_threads_model.dart';
 
 class StoryThreadsController {
-  CarouselSliderController pageController = CarouselSliderController();
+  PageController pageController = PageController();
   Map<String, StoryController> _controllers = {};
 
   // jumpTo(int index) => pageController.(index);
 
-  void nextThreads() => pageController.nextPage(Duration(milliseconds: 500));
+  void nextThreads() => pageController.nextPage(duration: Duration(milliseconds: 500), curve: Curves.linear);
 
-  void previousThreads() => pageController.previousPage(Duration(milliseconds: 500));
+  void previousThreads() => pageController.previousPage(duration : Duration(milliseconds: 500), curve: Curves.linear);
 
   StoryController findController(String id) => _controllers[id] ??= StoryController();
 
   void onPageChanged(StoryThreadsModel thread) {
-    _controllers.forEach((key, value) => value.pause());
-    findController(thread.id).play();
+    // _controllers.forEach((key, value) => value.pause());
+    // findController(thread.id).play();
   }
 }
--- lib/widgets/story_threads_view.dart
+++ lib/widgets/story_threads_view.dart
@@ -1,11 +1,11 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_carousel_slider/carousel_slider.dart';
 import 'package:story_view/models/story_events.dart';
 import '../controller/story_threads_controller.dart';
 import '../models/story_model.dart';
 import '../models/story_threads_model.dart';
 import 'story_view.dart';
+import 'dart:math' as math;
 
 class StoryThreadsView extends StatelessWidget {
   final StoryThreadsController controller;
@@ -38,41 +38,190 @@ class StoryThreadsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CarouselSlider.builder(
-        slideTransform: CubeTransform(),
-        onSlideChanged: (index) => controller.onPageChanged(threads[index]),
+      body: CubePageView(
+        onPageChanged: (index) => controller.onPageChanged(threads[index]),
         controller: controller.pageController,
-        unlimitedMode: true,
-        itemCount: threads.length,
-        slideBuilder: (i) => StoryView(
-          inline: true,
-          showShadow: true,
-          onPreviousPressed: controller.previousThreads,
-          controller: controller.findController(threads[i].id),
-          onComplete: () {
-            if (threads[i] == threads.last) {
-              onComplete?.call();
-            } else {
-              controller.nextThreads();
-            }
-          },
-          onStoryShow: onStoryShow,
-          retryButtonStyle: retryButtonStyle,
-          errorTextStyle: errorTextStyle,
-          indicatorForegroundColor: indicatorForegroundColor,
-          title: title,
-          avatar: avatar,
-          storyItems: [
-            for (int j = 0; j < threads.length; j++)
-              StoryItem.fromModel(
-                threads[i].stories[j],
-                controller.findController(threads[i].id),
-                onButtonPressed: onButtonPressed,
-                buttonStyle: buttonStyle,
-              )
-          ],
+        // itemCount: threads.length,
+        children: [
+          for(var model in threads)
+          StoryView(
+            inline: true,
+            showShadow: true,
+            onPreviousPressed: controller.previousThreads,
+            controller: controller.findController(model.id),
+            onComplete: () {
+              if (threads == threads.last) {
+                onComplete?.call();
+              } else {
+                controller.nextThreads();
+              }
+            },
+            onStoryShow: onStoryShow,
+            retryButtonStyle: retryButtonStyle,
+            errorTextStyle: errorTextStyle,
+            indicatorForegroundColor: indicatorForegroundColor,
+            title: title,
+            avatar: avatar,
+            storyItems: [
+              for (int j = 0; j < threads.length; j++)
+                StoryItem.fromModel(
+                  model.stories[j],
+                  controller.findController(model.id),
+                  onButtonPressed: onButtonPressed,
+                  buttonStyle: buttonStyle,
+                )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/// This Widget has the [PageView] widget inside.
+/// It works in two modes :
+///   1 - Using the default constructor [CubePageView] passing the items in `children` property.
+///   2 - Using the factory constructor [CubePageView.builder] passing a `itemBuilder` and `itemCount` properties.
+class CubePageView extends StatefulWidget {
+  /// Called whenever the page in the center of the viewport changes.
+  final ValueChanged<int> onPageChanged;
+
+  /// An object that can be used to control the position to which this page
+  /// view is scrolled.
+  final PageController controller;
+
+  /// Widgets you want to use inside the [CubePageView], this is only required if you use [CubePageView] constructor
+  final List<Widget> children;
+
+  /// Creates a scrollable list that works page by page from an explicit [List]
+  /// of widgets.
+  const CubePageView({
+    Key? key,
+    required this.onPageChanged,
+    required this.controller,
+    required this.children,
+  }) : super(key: key);
+
+  /// Creates a scrollable list that works page by page using widgets that are
+  /// created on demand.
+  ///
+  /// This constructor is appropriate if you want to customize the behavior
+  ///
+  /// Providing a non-null [itemCount] lets the [CubePageView] compute the maximum
+  /// scroll extent.
+  ///
+  /// [itemBuilder] will be called only with indices greater than or equal to
+  /// zero and less than [itemCount].
+  CubePageView.builder({
+    Key? key,
+    required this.onPageChanged,
+    required this.controller,
+    required this.children,
+  }) : super(key: key);
+
+  @override
+  _CubePageViewState createState() => _CubePageViewState();
+}
+
+class _CubePageViewState extends State<CubePageView> {
+  final _pageNotifier = ValueNotifier(0.0);
+  late PageController _pageController;
+
+  void _listener() {
+    _pageNotifier.value = _pageController.page!;
+  }
+
+  @override
+  void initState() {
+    _pageController = widget.controller;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _pageController.addListener(_listener);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.removeListener(_listener);
+    _pageController.dispose();
+    _pageNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Center(
+        child: ValueListenableBuilder<double>(
+          valueListenable: _pageNotifier,
+          builder: (_, value, child) => PageView.builder(
+            controller: _pageController,
+            onPageChanged: widget.onPageChanged,
+            physics: const ClampingScrollPhysics(),
+            itemCount: widget.children.length,
+            itemBuilder: (_, index) {
+              return CubeWidget(
+                child: widget.children[index],
+                index: index,
+                pageNotifier: value,
+              );
+            },
+          ),
         ),
       ),
     );
   }
 }
+
+/// This widget has the logic to do the 3D cube transformation
+/// It only should be used if you use [CubePageView.builder]
+class CubeWidget extends StatelessWidget {
+  /// Index of the current item
+  final int index;
+
+  /// Page Notifier value, it comes from the [CubeWidgetBuilder]
+  final double pageNotifier;
+
+  /// Child you want to use inside the Cube
+  final Widget child;
+
+  const CubeWidget({
+    Key? key,
+    required this.index,
+    required this.pageNotifier,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isLeaving = (index - pageNotifier) <= 0;
+    final t = (index - pageNotifier);
+    final rotationY = lerpDouble(0, 90, t);
+    final opacity = lerpDouble(0, 1, t.abs())?.clamp(0.0, 1.0);
+    final transform = Matrix4.identity();
+    transform.setEntry(3, 2, 0.003);
+    transform.rotateY(-degToRad(rotationY ?? 0));
+    return Transform(
+      alignment: isLeaving ? Alignment.centerRight : Alignment.centerLeft,
+      transform: transform,
+      child: Stack(
+        children: [
+          child,
+          Positioned.fill(
+            child: Opacity(
+              opacity: opacity ?? 1,
+              child: Container(
+                child: Container(
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+double degToRad(num deg) => deg * (math.pi / 180.0);
