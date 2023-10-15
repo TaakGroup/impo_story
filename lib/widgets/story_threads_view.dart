316578c4120f47d0ec918520d8681f363bb1f4c7
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-10-15 09:50:19 +0330

    Threads

--- lib/widgets/story_threads_view.dart
+++ lib/widgets/story_threads_view.dart
@@ -196,7 +196,7 @@ class CubeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final isLeaving = (index - pageNotifier) <= 0;
     final t = (index - pageNotifier);
-    final rotationY = lerpDouble(0, 60, -t);
+    final rotationY = lerpDouble(0, -60, -t);
     final opacity = lerpDouble(0, 1, t.abs())?.clamp(0.0, 1.0);
     final transform = Matrix4.identity();
     transform.setEntry(3, 2, 0.003);
